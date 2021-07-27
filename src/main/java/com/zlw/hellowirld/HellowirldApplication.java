f203ab69575c3f9b73fa0f9471a64935ddf4f246
VVIZE-ops <55495676+VVIZE-ops@users.noreply.github.com>
2021-07-27 18:28:43 +0800

    Add files via upload

---
+++ src/main/java/com/zlw/hellowirld/HellowirldApplication.java
@@ -0,0 +1,13 @@
+package com.zlw.hellowirld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowirldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowirldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlw/hellowirld/HellowirldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlw.hellowirld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowirldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
